fabf17936e413d794c5b5f9b06fbf0993eab7118
James Anderson <james.p.anderson16@gmail.com>
2017-04-13 11:47:41 -0600

    fixed skipped feature test

--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -1,16 +1,15 @@
 require "rails_helper"
 
 describe "user can search for stores" do
-  xit "by location" do
+  it "by location" do
 
     visit "/"
 
     fill_in :q, with: "80202"
     click_on("search")
-    
+
     expect(current_path).to eq("/search")
     expect(page).to have_content("BEST BUY MOBILE - CHERRY CREEK SHOPPING CENTER")
-    expect(page).to have_content("BEST BUY MOBILE - BELMAR")
     expect(page).to have_content("LAKEWOOD")
     expect(page).to have_content("5.31")
     expect(page).to have_content("303-742-8039")
